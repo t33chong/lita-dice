54801d6647a9dea20685f082f4a73b21a4414a2a
Tristan Euan Chong <tristaneuan@gmail.com>
2014-01-17 23:17:28 -0800

    Added roll route

--- lib/lita/handlers/dice.rb
+++ lib/lita/handlers/dice.rb
@@ -3,6 +3,11 @@ require "lita"
 module Lita
   module Handlers
     class Dice < Handler
+      route %r{roll}i, :roll, command: true, help: {'roll' => 'Roll one die'}
+
+      def roll(response)
+        response.reply (1..6).to_a.sample
+      end
     end
 
     Lita.register_handler(Dice)
