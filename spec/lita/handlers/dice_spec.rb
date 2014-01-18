1ab5bb9635521f9f39489cd5690d9b27e1dcd4f8
Tristan Euan Chong <tristaneuan@gmail.com>
2014-01-17 23:51:55 -0800

    added another test

--- spec/lita/handlers/dice_spec.rb
+++ spec/lita/handlers/dice_spec.rb
@@ -2,4 +2,11 @@ require "spec_helper"
 
 describe Lita::Handlers::Dice, lita_handler: true do
   it { routes_command("roll").to(:roll) }
+
+  describe "#roll" do
+    it "replies with a random number from 1 to 6" do
+      send_message "roll"
+      expect(replies).to include((1..6).to_a.sample.to_s)
+    end
+  end
 end
