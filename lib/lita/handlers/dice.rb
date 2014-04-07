275bf2630810b9a86c5103297f95ef1e99d4c84a
Tristan Chong <tristanchong@gmail.com>
2014-04-06 22:14:55 -0700

    add rollx

--- lib/lita/handlers/dice.rb
+++ lib/lita/handlers/dice.rb
@@ -3,7 +3,23 @@ require "lita"
 module Lita
   module Handlers
     class Dice < Handler
-      route(/roll/i, :roll, command: true, help: {'roll' => 'Roll one die'})
+      route(/^roll\s+(\d+)/i, :rollx, command: true, help: {
+        'roll X' => 'Roll X dice'})
+
+      route(/^roll\s+[^\d]/i, :roll, command: true, help: {
+        'roll' => 'Roll one die'})
+
+      def rollx(response)
+        x = response.matches.first.first.to_i
+        if x > 20
+          return
+        end
+        s = ''
+        (1..x).each do |n|
+          s += (1..6).to_a.sample.to_s + ' '
+        end
+        response.reply s
+      end
 
       def roll(response)
         response.reply (1..6).to_a.sample.to_s
