4465df3fbadf904f2c4bd78a73daa31b1d4e06b5
Tristan Chong <tristanchong@gmail.com>
2014-04-18 23:19:15 -0700

    improved route, single roll method, return total

--- lib/lita/handlers/dice.rb
+++ lib/lita/handlers/dice.rb
@@ -3,47 +3,39 @@ require "lita"
 module Lita
   module Handlers
     class Dice < Handler
-      route(/^roll\s+(\d+)d(\d+)/i, :rollxdy, command: true, help: {
+      route(/^roll(\s+(?<x>\d+)?d?(?<y>\d+)?)?/i, :roll, command: true, help: {
+        'roll' => 'Roll one 6-sided die',
+        'roll X' => 'Roll X 6-sided dice',
         'roll XdY' => 'Roll X Y-sided dice'})
 
-      route(/^roll\s+(\d+)$/i, :rollx, command: true, help: {
-        'roll X' => 'Roll X 6-sided dice'})
-
-      route(/^roll\s+[^\d]/i, :roll, command: true, help: {
-        'roll' => 'Roll one 6-sided die'})
-
-      def rollxdy(response)
-        x, y = response.matches.first
-        x = x.to_i
-        y = y.to_i
-        if x > 20 or x < 1
-          s = 'You can only roll between 1 and 20 dice.'
-        else
-          s = response.user.name + ' rolled '
-          (1..x).each do |n|
-            s += (1..y).to_a.sample.to_s + ' '
-          end
-        end
-        response.reply s
-      end
-
-      def rollx(response)
-        x = response.matches.first.first.to_i
-        if x > 20 or x < 1
-          s = 'You can only roll between 1 and 20 dice.'
-        else
-          s = response.user.name + ' rolled '
-          (1..x).each do |n|
-            s += (1..6).to_a.sample.to_s + ' '
-          end
-        end
-        response.reply s
-      end
-
       def roll(response)
-        s = response.user.name + ' rolled '
-        s += (1..6).to_a.sample.to_s
+        int = lambda do |n, default|
+          if n == nil
+            return default
+          end
+          n.to_i
+        end
+
+        x, y = response.matches.captures
+        x = int.call(x, 1)
+        y = int.call(y, 6)
+
+        if x > 20 or x < 1
+          s = 'You can only roll between 1 and 20 dice.'
+        elsif y > 1000 or y < 2
+          s = 'Dice must have between 2 and 1000 sides.'
+        else
+          rolls = []
+          (1..x).each do |n|
+            rolls << (1..y).to_a.sample
+          end
+          s = "#{response.user.name} rolled #{rolls.join(' ')}"
+          if x > 1
+            total = rolls.inject(:+)
+            s += " (#{total})"
+          end
         response.reply s
+        end
       end
     end
 
