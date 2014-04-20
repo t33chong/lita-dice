05dfcf9d03154ed5c2ff0cc07b5f5ea44e3f84bc
Tristan Chong <tristanchong@gmail.com>
2014-04-19 17:10:59 -0700

    test roll x

--- spec/lita/handlers/dice_spec.rb
+++ spec/lita/handlers/dice_spec.rb
@@ -8,5 +8,14 @@ describe Lita::Handlers::Dice, lita_handler: true do
       send_command "roll"
       expect("1".."6").to include(replies.first)
     end
+
+    it "replies with as many random numbers from 1 to 6 as specified" do
+      send_command "roll 3"
+      re = /rolled ([1-6]) ([1-6]) ([1-6]) \((\d+)\)/
+      expect(replies.first).to match(re)
+      capt = re.match(replies.first).captures
+      a, b, c, total = capt.map { |n| n.to_i }
+      expect(a + b + c).to equal(total)
+    end
   end
 end
