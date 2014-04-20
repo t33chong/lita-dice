0b5549c6c82ff6faf295cd3a2b6566cbeedb3629
Tristan Chong <tristanchong@gmail.com>
2014-04-19 17:37:49 -0700

    add username to regex

--- spec/lita/handlers/dice_spec.rb
+++ spec/lita/handlers/dice_spec.rb
@@ -4,14 +4,14 @@ describe Lita::Handlers::Dice, lita_handler: true do
   it { routes_command("roll").to(:roll) }
 
   describe "#roll" do
-    it "replies with a random number from 1 to 6" do
-      send_command "roll"
-      expect("1".."6").to include(replies.first)
-    end
+    #it "replies with a random number from 1 to 6" do
+    #  send_command "roll"
+    #  expect("1".."6").to include(replies.first)
+    #end
 
-    it "replies with as many random numbers from 1 to 6 as specified" do
+    it "replies with the specified number of rolls and the correct total" do
       send_command "roll 3"
-      re = /rolled ([1-6]) ([1-6]) ([1-6]) \((\d+)\)/
+      re = /#{user.name} rolled ([1-6]) ([1-6]) ([1-6]) \((\d+)\)/
       expect(replies.first).to match(re)
       capt = re.match(replies.first).captures
       a, b, c, total = capt.map { |n| n.to_i }
